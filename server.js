8db6f14d62426fd6e5decfe177f5d66bbcb52b9c
Cetins <69481095+Cetins@users.noreply.github.com>
2023-01-14 15:28:09 +0000

    just trying to alter client connection

--- server.js
+++ server.js
@@ -10,14 +10,6 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 client.connect(err => {
   const collection = client.db("test").collection("devices");
   // perform actions on the collection object
-  client.close();
-});
-
-app.use(express.json());
-app.use(cors());
-
-MongoClient.connect('mongodb://127.0.0.1:27017', { useUnifiedTopology: true })
-.then((client) => {
   const db = client.db('sale_tracker');
 
   const shopCollection = db.collection('shop');
@@ -37,9 +29,36 @@ MongoClient.connect('mongodb://127.0.0.1:27017', { useUnifiedTopology: true })
   app.use('/api/services', servicesRouter);
   app.use('/api/staff', staffRouter);
   app.use('/api/sales', salesRouter);
-})
-.catch(console.error);
-
-app.listen(9000, function () {
-  console.log('Listening Sales Tracker on port ${ this.address().port }');
+  client.close();
 });
+
+app.use(express.json());
+app.use(cors());
+
+// MongoClient.connect('mongodb://127.0.0.1:27017', { useUnifiedTopology: true })
+// .then((client) => {
+//   const db = client.db('sale_tracker');
+
+//   const shopCollection = db.collection('shop');
+//   const productsCollection = db.collection('products');
+//   const servicesCollection = db.collection('services');
+//   const staffCollection = db.collection('staff');
+//   const salesCollection = db.collection('sales');
+
+//   const shopRouter = createRouter(shopCollection);
+//   const productsRouter = createRouter(productsCollection);
+//   const servicesRouter = createRouter(servicesCollection);
+//   const staffRouter = createRouter(staffCollection);
+//   const salesRouter = createRouter(salesCollection);
+
+//   app.use('/api/shop', shopRouter);
+//   app.use('/api/products', productsRouter);
+//   app.use('/api/services', servicesRouter);
+//   app.use('/api/staff', staffRouter);
+//   app.use('/api/sales', salesRouter);
+// })
+// .catch(console.error);
+
+// app.listen(9000, function () {
+//   console.log('Listening Sales Tracker on port ${ this.address().port }');
+// });

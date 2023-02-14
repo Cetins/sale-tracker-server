db59deefa71cf895e51204199cbd0b3d480a93a8
Cetins <69481095+Cetins@users.noreply.github.com>
2023-02-14 14:46:20 +0000

    changed the structure again...

--- db/seeds.js
+++ db/seeds.js
@@ -1,34 +1,199 @@
 use sale_tracker;
 db.dropDatabase();
 
+// db.users.insertMany([
+//     {
+//         first_name: "Michael",
+//         last_name: "Purple",
+//         email: "test@mail.com",
+//         admin_pascode: 1234,
+//         shop: {
+//                 name: "Test Shop",
+//                 address: "123 Green Street T12 3TT",
+//                 phone: "0123456789",     
+//             },
+//         staff: [
+//             {
+//                 first_name: "Michael",
+//                 last_name: "Purple",
+//                 email: "mpurple@mail.com",
+//                 phone: "07770888999",
+//                 payment_category: "Business Owner",
+//             },
+//             {
+//                 first_name: "Jennifer",
+//                 last_name: "Blue",
+//                 email: "jblue@mail.com",
+//                 phone: "07770888999",
+//                 payment_category: "Self Employed",
+//                 service_commission_percentage: 50,
+//                 product_commission_percentage: 5
+//             },
+//             {
+//                 first_name: "Pamela",
+//                 last_name: "Yellow",
+//                 email: "pyellow@mail.com",
+//                 phone: "07770888999",
+//                 payment_category: "Wages",
+//                 wages_per_hour: 9.50,
+//             }
+//         ],
+//         services: [
+//             {
+//                 title: "Cleaning",
+//                 duration: 50,
+//                 price: 30
+//             },
+//             {
+//                 title: "Laundry",
+//                 duration: 40,
+//                 price: 25
+//             }
+//         ],
+//         products: [
+//             {
+//                 title: "Glass Spray",
+//                 manufacturer: "Glass ltd",
+//                 price: 18,
+//                 cost: 9,
+//                 stock: 23,
+//                 sold: 0
+//             },
+//             {
+//                 title: "Laundry Pods",
+//                 manufacturer: "Laundry ltd",
+//                 price: 13,
+//                 cost: 6.5,
+//                 stock: 14,
+//                 sold: 0
+//             },
+//             {
+//                 title: "Microfiber Cloths",
+//                 manufacturer: "Cloth ltd",
+//                 price: 5,
+//                 cost: 2.5,
+//                 stock: 3,
+//                 sold: 0
+//             }
+//         ]
+//     },
+//     {
+//         email: "demo@mail.com",
+//         shop: {
+//                 name: "Demo Shop",
+//                 address: "123 Green Street T12 3TT",
+//                 phone: "0123456789",
+//                 branchName: "GreenStreet",
+//                 sales_database: null
+//             },
+//         staff: [
+//             {
+//                 first_name: "Michael",
+//                 last_name: "Purple",
+//                 email: "mpurple@mail.com",
+//                 phone: "07770888999",
+//                 admin_access: true,
+//                 admin_pascode: 1234,
+//                 payment_category: "Business Owner",
+//             },
+//             {
+//                 first_name: "Jennifer",
+//                 last_name: "Blue",
+//                 email: "jblue@mail.com",
+//                 phone: "07770888999",
+//                 admin_access: false,
+//                 payment_category: "Self Employed",
+//                 service_commission_percentage: 50,
+//                 product_commission_percentage: 5
+//             },
+//             {
+//                 first_name: "Pamela",
+//                 last_name: "Yellow",
+//                 email: "pyellow@mail.com",
+//                 phone: "07770888999",
+//                 admin_access: false,
+//                 payment_category: "Wages",
+//                 wages_per_hour: 9.50,
+//             }
+//         ],
+//         services: [
+//             {
+//                 title: "Cleaning",
+//                 duration: 50,
+//                 price: 30
+//             },
+//             {
+//                 title: "Laundry",
+//                 duration: 40,
+//                 price: 25
+//             }
+//         ],
+//         products: [
+//             {
+//                 title: "Glass Spray",
+//                 manufacturer: "Glass ltd",
+//                 price: 18,
+//                 cost: 9,
+//                 stock: 23,
+//                 sold: 0
+//             },
+//             {
+//                 title: "Laundry Pods",
+//                 manufacturer: "Laundry ltd",
+//                 price: 13,
+//                 cost: 6.5,
+//                 stock: 14,
+//                 sold: 0
+//             },
+//             {
+//                 title: "Microfiber Cloths",
+//                 manufacturer: "Cloth ltd",
+//                 price: 5,
+//                 cost: 2.5,
+//                 stock: 3,
+//                 sold: 0
+//             }
+//         ]
+//     }
+// ]);
+
+// db.sales.insertMany([]);
+
+
 db.shop.insertMany([
     {
-        name: "Brush Barbers",
-        email: "brushbarbers@mail.com",
+        name: "Demo Barbers",
+        email: "demo@mail.com",
         passCode: "1234"
     }
 ])
 
 db.staff.insertMany([
     {
-        name: "Husso",
-        email: "huseyinsezencetin@gmail.com",
-        category: "Self-employed",
-        service_percentage: 0,
-        product_percentage: 0,
+        username: "Heather",
+        first_name: "Heather",
+        last_name: "Blue",
+        email: "heather@mail.com",
+        category: "Business Owner",
+        service_commission_percentage: 0,
+        product_commission_percentage: 0,
     },
     {
-        name: "Tracey",
-        email: "tracey@mail.com",
-        category: "Self-employed",
-        service_percentage: 50,
+        username: "Amanda",
+        first_name: "Amanda",
+        last_name: "Green",
+        email: "amanda@mail.com",
+        category: "Self Employed",
+        service_commission_percentage: 50,
     },
     {
-        name: "Christine",
-        email: "chris@mail.com",
+        username: "Penny",
+        first_name: "Penny",
+        last_name: "Violet",
+        email: "penny@mail.com",
         category: "Wages",
         wages_per_hour: 9.5,
-        product_percentage: 5,
+        product_commission_percentage: 5,
     }
 ])
 
